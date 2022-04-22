81d3827e4705938aa9f328c1a3134a0a07a7a54c
Francis Okonkwo <francisalexander000@gmail.com>
2022-04-22 12:24:32 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function validateForm() {
+alert('Successful Submission!😎');
+}
+
+function increment(){
+    document.getElementById('num1').stepUp();
+}
+
+function decrement(){
+    document.getElementById('num1').stepDown();
+}
+
+function increase(){
+    document.getElementById('num2').stepUp();
+}
+
+function decrease(){
+    document.getElementById('num2').stepDown();
+}
